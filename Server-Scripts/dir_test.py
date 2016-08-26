e06335fe68b14632aaf86bbf710b8a81d77fdc08
Levis Jani <levisjani@rocketmail.com>
2016-08-26 18:10:03 +0200

    Create dir_test.py

---
+++ Server-Scripts/dir_test.py
@@ -0,0 +1,8 @@
+# Script Name		: dir_test.py
+# Author				: Levis Jani
+
+# Description			: Tests to see if the directory testdir exists, if not it will create the directory for you
+
+import os									# Import the OS module
+if not os.path.exists('testdir'):		#  Check to see if it exists  
+  os.makedirs('testdir')				#  Create the directory 
